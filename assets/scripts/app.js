202a9c1b60174afcbea875a62677cbc6da9e5109
KnockOut19882 <mpodolski1988@gmail.com>
2025-11-04 21:14:02 +0100

    add strongAttackHandler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,24 @@
-const ATTACK_DMG = 10;
+const PLAYER_ATTACK_DMG = 10;
+const MONSTER_ATTACK_DMG = 14;
 
-let chosenMaxLife = 100;
-let currentMonsterHealth = chosenMaxLife;git 
+let chosenMaxLife = 5;
+let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler () {
-    const damage = dealMonsterDamage(ATTACK_DMG);
+    const damage = dealMonsterDamage(PLAYER_ATTACK_DMG);
     currentMonsterHealth -= damage;
+    const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_DMG);
+    currentPlayerHealth -= monsterDamage
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lose');
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("It's a DRAW");
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
