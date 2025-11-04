a8adf7a787db4dcc1bb27057bc8735a70c5624dd
KnockOut19882 <mpodolski1988@gmail.com>
2025-11-04 22:00:31 +0100

    add attackMonster function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,54 @@
 const PLAYER_ATTACK_DMG = 10;
 const MONSTER_ATTACK_DMG = 14;
+const STRONG_ATTACK_DMG = 17;
 
-let chosenMaxLife = 5;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler () {
-    const damage = dealMonsterDamage(PLAYER_ATTACK_DMG);
-    currentMonsterHealth -= damage;
-    const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_DMG);
-    currentPlayerHealth -= monsterDamage
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert('You won');
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You lose');
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-        alert("It's a DRAW");
-    }
+function attackMonster(mode) {
+	if (mode === PLAYER_ATTACK_DMG) {
+		const damage = dealMonsterDamage(PLAYER_ATTACK_DMG);
+		currentMonsterHealth -= damage;
+		const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_DMG);
+		currentPlayerHealth -= monsterDamage;
+	} else {
+		const damage = dealMonsterDamage(STRONG_ATTACK_DMG);
+		currentMonsterHealth -= damage;
+		const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_DMG);
+		currentPlayerHealth -= monsterDamage;
+	}
+}
+
+function attackHandler() {
+	const damage = dealMonsterDamage(PLAYER_ATTACK_DMG);
+	currentMonsterHealth -= damage;
+const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_DMG);
+currentPlayerHealth -= monsterDamage;
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+		alert('You won');
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert('You lose');
+	} else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+		alert("It's a DRAW");
+	}
+}
+
+function strongAttackHandler() {
+	const damage = dealMonsterDamage(STRONG_ATTACK_DMG);
+	currentMonsterHealth -= damage;
+	const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_DMG);
+	currentPlayerHealth -= monsterDamage;
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+		alert('You won');
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert('You lose');
+	} else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+		alert("It's a DRAW");
+	}
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
